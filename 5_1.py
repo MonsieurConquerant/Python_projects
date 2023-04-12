61ebe071640ad5a20ec604f83f494bda6f7f890f
Adilzhan Samigulov <a.samighulov2@gmail.com>
2023-04-13 02:57:43 +0600

    lab 1 rdy

--- 5_1.py
+++ 5_1.py
@@ -1,1 +1,19 @@
-1234567
+# Напишите рекурсивную функцию для возведения числа a в степень b. 
+# Разрешается использовать только операцию умножения. 
+# Циклы использовать нельзя
+print('введите а: ')
+a = int(input())
+print('введите b: ')
+b = int(input())
+
+def recursion(a, b):
+    if b == 0:
+        return 1
+    if b < 0:
+        return 1 / recursion(a, -b)
+    if b % 2 == 0:
+        return recursion(a, b // 2) * recursion(a, b // 2)
+    else:
+        return recursion(a, b - 1) * a
+    
+print(recursion(a, b))
